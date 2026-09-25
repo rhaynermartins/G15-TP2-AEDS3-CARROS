a5d3a37efd6c41df3ca8b540dcbfef6d484d6616
rhaynermartins <rhayner2005@hotmail.com>
2026-09-25 19:35:55 -0300

    Inclui dependências e testes TP2 na compilação nativa

--- compilar.bat
+++ compilar.bat
@@ -1,5 +1,5 @@
 @echo off
 if not exist out mkdir out
-javac -encoding UTF-8 -d out src\Main.java src\TesteTP1.java src\model\Carro.java src\dao\ArquivoSequencial.java src\service\Importador.java src\service\OrdenacaoExterna.java src\util\CsvUtil.java
+javac -encoding UTF-8 -d out -sourcepath src src\*.java
 if %errorlevel% neq 0 exit /b %errorlevel%
 echo Compilacao concluida.
