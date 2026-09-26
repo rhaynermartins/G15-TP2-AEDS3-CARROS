c9773dec05482868fd7334b7be34d32ab94c746b
rhaynermartins <rhayner2005@hotmail.com>
2026-09-26 12:34:03 -0300

    Inclui as suítes do TP2 no atalho de testes

--- testar.bat
+++ testar.bat
@@ -1,2 +1,6 @@
 @echo off
-java -cp out TesteTP1
+for %%T in (TesteTP1 TesteBPlus TesteHash TesteListas TesteFase2 TesteTP2) do (
+    java -Xmx512m -cp out %%T
+    if errorlevel 1 exit /b 1
+)
+echo Todas as suites funcionais passaram.
